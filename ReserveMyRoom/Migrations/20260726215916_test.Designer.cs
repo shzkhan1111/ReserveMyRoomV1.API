fe612d608eda58d83ca4deb0098a637b80c74a8d
shahzaib.ahmed <shahzaib.ahmedkhan111@gmail.com>
2026-07-26 23:26:25 +0100

    Applying New Migration

--- ReserveMyRoom/Migrations/20260726215916_test.Designer.cs
+++ ReserveMyRoom/Migrations/20260726215916_test.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ReserveMyRoom.API.Data;
 
@@ -11,9 +12,11 @@ using ReserveMyRoom.API.Data;
 namespace ReserveMyRoom.API.Migrations
 {
     [DbContext(typeof(ReserveMyRoomDbContext))]
-    partial class ReserveMyRoomDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260726215916_test")]
+    partial class test
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
